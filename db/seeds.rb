6a8af8b67f0141ece225da08cb4fd9ed158359be
karim issa <issa61139@gmail.com>
2024-04-13 15:25:21 -0400

    implemented the routes, controllers, and the simple_form

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.create!([
+  { name: "The Fancy Fork", address: "123 Main St, Montreal", phone_number: "123-456-7890", category: "french" },
+  { name: "Sushi Sushi", address: "456 Sushi Rd, Montreal", phone_number: "234-567-8901", category: "japanese" },
+  { name: "Pasta Central", address: "789 Pasta Blvd, Montreal", phone_number: "345-678-9012", category: "italian" },
+  { name: "Dragon Wok", address: "012 China Town, Montreal", phone_number: "456-789-0123", category: "chinese" },
+  { name: "Waffle Kingdom", address: "345 Waffle Way, Montreal", phone_number: "567-890-1234", category: "belgian" }
+])
