e3559c119d469ea6d794d0832ec5f82ce81d223d
karim issa <issa61139@gmail.com>
2024-04-13 15:36:34 -0400

    refactured the code

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,9 +8,11 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = @restaurant.reviews.build(review_params)
     if @review.save
-      redirect_to restaurant_path(@restaurant)
+      redirect_to restaurant_path(@restaurant), notice: 'Review added successfully.'
     else
-      render :new
+      @reviews = @restaurant.reviews.reload
+      flash.now[:alert] = 'Failed to add review. Please check the form for errors.'
+      render 'restaurants/show'
     end
   end
 
